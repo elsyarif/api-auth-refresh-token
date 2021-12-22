c26bef5ccb539ae830c4a09d73d204999e13a1cf
Syarif Hidayatulloh <id.syarif@gmail.com>
2021-12-22 15:32:02 +0700

    service

---
+++ src/services/authServices.js
@@ -0,0 +1,16 @@
+class userService{
+
+    async find(params){}
+
+    async get(id, params){}
+
+    async create(data, params){}
+
+    async update(id, data, params){}
+
+    async patch(id, data, params){}
+    
+    async remove(id, params){}
+}
+
+export default userService
